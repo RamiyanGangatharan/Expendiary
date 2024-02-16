600493d00bbb639060df71c1d9cd195dbf2ead5b
Ramiyan Gangatharan <135138789+RamiyanGangatharan@users.noreply.github.com>
2024-02-16 01:07:27 -0500

    Initial #2

---
+++ UI/Reports.cs
@@ -0,0 +1,15 @@
+﻿namespace Expendiary.UI
+{
+    public partial class Reports : Form
+    {
+        public Reports()
+        {
+            InitializeComponent();
+        }
+
+        private void Reports_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
