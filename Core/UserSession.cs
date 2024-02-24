def53949bcfe58945adf39d962bb485c531f595e
Ramiyan Gangatharan <135138789+RamiyanGangatharan@users.noreply.github.com>
2024-02-24 11:37:35 -0500

    Core Files

---
+++ Core/UserSession.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expendiary.Core
+{
+    public static class UserSession
+    {
+        public static string CurrentUsername { get; private set; }
+
+        public static void Login(string username)
+        {
+            CurrentUsername = username;
+        }
+
+        public static void Logout()
+        {
+            CurrentUsername = null;
+        }
+    }
+}
