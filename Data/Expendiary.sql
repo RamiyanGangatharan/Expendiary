fbf0d9dfb3f02e0add58901d963d12b6d4781012
Ramiyan Gangatharan <135138789+RamiyanGangatharan@users.noreply.github.com>
2024-02-24 11:38:12 -0500

    Data and Object Files

--- Data/Expendiary.sql
+++ Data/Expendiary.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users
+﻿CREATE TABLE users
 (
     userID INT IDENTITY(1,1) PRIMARY KEY,
     username VARCHAR(25) UNIQUE NOT NULL,
@@ -12,21 +12,12 @@ CREATE TABLE transactions
     transactionID INT IDENTITY(1,1) PRIMARY KEY,
     username VARCHAR(25) REFERENCES users(username),
     company VARCHAR(100) NOT NULL,
-    category VARCHAR(50) CHECK (category IN ('Housing', 'Transportation', 'Dining', 'Utilities', 'Entertainment', 'Grocery', 'Fuel', 'Insurance', 'Medical', 'Savings', 'Investments', 'Miscellaneous')),
+    category VARCHAR(50) CHECK (category IN ('Income', 'Housing', 'Transportation', 'Dining', 'Utilities', 'Entertainment', 'Grocery', 'Fuel', 'Insurance', 'Medical', 'Savings', 'Investments', 'Miscellaneous')),
     amount DECIMAL(10,2) NOT NULL,
     dateOfPurchase DATE NOT NULL
 );
 
-DROP TABLE users;
-SELECT * FROM users;
+SELECT * FROM transactions;
+DELETE FROM users WHERE users.userID = 1;
 
-SELECT dateOfPurchase AS 'Date',
-       transactionID AS 'TransactionID',
-       username AS 'Username',
-       category AS 'Category',
-       company AS 'Company',
-       amount AS 'Amount'
-FROM transactions
-ORDER BY dateOfPurchase, transactionID;
-
-DELETE FROM users WHERE userID = 2;
+DROP TABLE transactions;
