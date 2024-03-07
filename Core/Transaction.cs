4b94b8c947b5085066a6ab16f800c349559156d0
Ramiyan Gangatharan <135138789+RamiyanGangatharan@users.noreply.github.com>
2024-03-07 09:40:20 -0500

    Core Files

---
+++ Core/Transaction.cs
@@ -0,0 +1,32 @@
+﻿namespace Expendiary.Core
+{
+    internal class Transaction
+    {
+        public int TransactionID { get; set; }
+        public string Username { get; set; } 
+        public string Company { get; set; } 
+        public string Category { get; set; } 
+        public decimal Amount { get; set; } 
+        public DateTime DateOfPurchase { get; set; } 
+
+        public Transaction()
+        {
+            TransactionID = 0;
+            Username = "";
+            Company = "";
+            Category = "";
+            Amount = 0.0m;
+            DateOfPurchase = DateTime.UtcNow;
+        }
+
+        public Transaction(int transactionID, string username, string company, string category, decimal amount, DateTime dateOfPurchase)
+        {
+            TransactionID = transactionID;
+            Username = username;
+            Company = company;
+            Category = category;
+            Amount = amount;
+            DateOfPurchase = dateOfPurchase;
+        }
+    }
+}
