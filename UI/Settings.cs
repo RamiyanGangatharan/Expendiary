434b274933282cb40f0a65ae0bf138075600f7a7
Ramiyan Gangatharan <135138789+RamiyanGangatharan@users.noreply.github.com>
2024-03-07 10:34:32 -0500

    User Interface Files

---
+++ UI/Settings.cs
@@ -0,0 +1,15 @@
+﻿namespace Expendiary.UI
+{
+    public partial class Settings : Form
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void Settings_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
